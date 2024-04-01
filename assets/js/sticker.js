b09330fb1208566c4c593bee8e71fa9976f4d09a
nuclearlala <165473806+nuclearlala@users.noreply.github.com>
2024-03-31 21:10:19 -0400

    drag test

---
+++ assets/js/sticker.js
@@ -0,0 +1,10 @@
+function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
